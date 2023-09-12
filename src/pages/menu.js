751722297d79b764a11a18290970dcd911a24074
NgoHa <ngominhha0843@gmail.com>
2023-09-12 20:48:09 +0700

    new website vic

---
+++ src/pages/menu.js
@@ -0,0 +1,133 @@
+const menus = [
+    {
+        id: 1,
+        name: 'TRANG CHỦ',
+        links: '/'
+    },
+    {
+        id: 2,
+        name: 'VỀ CHÚNG TÔI',
+        links: '#',
+        namesub: [
+            {
+                id: 1,
+                sub: 'HỆ THỐNG VICTORIA ENGLISH',
+                links: '/nfts'
+            },
+            {
+                id: 2,
+                sub: 'TẦM NHÌN & SỨ MỆNH',
+                links: '/blog'
+            },
+            {
+                id: 3,
+                sub: 'GIÁ TRỊ CỐT LÕI',
+                links: '/blog-details'
+            },
+            {
+                id: 4,
+                sub: 'PHƯƠNG CHÂM',
+                links: '/visions-mission'
+            },
+            {
+                id: 5,
+                sub: 'QUY MÔ HOẠT ĐỘNG',
+                links: '/help-center'
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: 'KHÓA HỌC',
+        links: '#',
+        namesub: [
+            {
+                id: 1,
+                sub: 'TIẾNG ANH DÀNH CHO TRẺ EM',
+                links: '#'
+            },
+            {
+                id: 2,
+                sub: 'TIẾNG ANH DÀNH CHO THANH THIẾU NIÊN',
+                links: '/blog'
+            },
+            {
+                id: 3,
+                sub: 'TIẾNG ANH CHUẨN ĐẦU RA TỐT NGHIỆP',
+                links: '/blog-details'
+            },
+            {
+                id: 4,
+                sub: 'ĐÀO TẠO TIẾNG ANH CHUẨN QUỐC TẾ CAMBRIDGE',
+                links: '/visions-mission'
+            },
+            {
+                id: 5,
+                sub: 'TIẾNG ANH DÀNH CHO DOANH NGHIỆP',
+                links: '/help-center'
+            },
+            {
+                id: 6,
+                sub: 'HỆ THỐNG MẦM NON VICTORIA KINDERWORLD',
+                links: '/help-center'
+            },
+            {
+                id: 7,
+                sub: 'DỰ BỊ TIỂU HỌC – HÀNH TRANG VÀO LỚP 1',
+                links: '/help-center'
+            },
+            {
+                id: 8,
+                sub: 'VICTORIA DU HỌC',
+                links: '/help-center'
+            }
+        ]
+    },
+    {
+        id: 4,
+        name: 'HỆ THỐNG CHI NHÁNH',
+        links: '#',
+        namesub: [
+            {
+                id: 1,
+                sub: 'VICTORIA NAM ĐỊNH',
+                links: '/blog-details'
+            },
+            {
+                id: 2,
+                sub: 'VICTORIA QUẢNG NINH',
+                links: '/blog-details'
+            },
+            {
+                id: 3,
+                sub: 'VICTORIA ĐÀ NẴNG',
+                links: '/blog-details'
+            },
+            {
+                id: 4,
+                sub: 'VICTORIA QUẢNG NGÃI',
+                links: '/blog-details'
+            },
+            {
+                id: 5,
+                sub: 'VICTORIA NHA TRANG',
+                links: '/blog-details'
+            }
+        ]
+    },
+    {
+        id: 6,
+        name: 'TIN TỨC',
+        links: '/blog',
+        
+    },
+    {
+        id: 7,
+        name: 'LIÊN HỆ',
+        links: '/contact',
+    },
+
+    
+]
+
+export default menus;
